fc05b30b00543016d412d85b8600834f2e38aae1
hasanicli <iclihasan@gmail.com>
2020-12-03 22:08:53 +0300

    birkaç değişiklik

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import sys
+
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 from create_main_page import MainWindow
+from create_staff_page import StaffWindow
 
 
 class App(MainWindow):
@@ -8,6 +11,12 @@ class App(MainWindow):
         super().__init__()
         print("")
         # Triggered signals
+        self.ui.act_add_staff.triggered.connect(lambda: self.open(StaffWindow))
+
+    def open(self, param_class):
+        self.inst = param_class()
+        self.inst.setWindowModality(Qt.ApplicationModal)
+        self.inst.show()
 
 
 if __name__ == "__main__":
