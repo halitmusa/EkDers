c4d27a6e82980bd33a90002e00ded370526b3636
hasanicli <iclihasan@gmail.com>
2020-12-07 12:22:52 +0300

    bir kaç bug giderildi. program exe haline dönüştürüldü.

--- create_main_page.py
+++ create_main_page.py
@@ -94,51 +94,54 @@ class MainWindow(QMainWindow):
         return int(obj.text())
 
     def calculator(self):
-        year_name = self.ui.cmb_year.currentText()
-        period_name = self.ui.cmb_period.currentText()
-        year_id = self.connection.find(f"""SELECT id FROM years WHERE name="{year_name}" """)
-        period_id = self.connection.find(f"""SELECT id FROM periods WHERE name="{period_name}" """)
-        multipliers = list(self.connection.selector(f"""SELECT * FROM data WHERE yearID={year_id} AND periodID={period_id}""")[0])
+        try:
+            year_name = self.ui.cmb_year.currentText()
+            period_name = self.ui.cmb_period.currentText()
+            year_id = self.connection.find(f"""SELECT id FROM years WHERE name="{year_name}" """)
+            period_id = self.connection.find(f"""SELECT id FROM periods WHERE name="{period_name}" """)
+            multipliers = list(self.connection.selector(f"""SELECT * FROM data WHERE yearID={year_id} AND periodID={period_id}""")[0])
 
-        price_coefficient = multipliers[2]
-        day_coefficient = multipliers[4]
-        night_coefficient = multipliers[3]
-        tax_rate = multipliers[5]
+            price_coefficient = multipliers[2]
+            day_coefficient = multipliers[4]
+            night_coefficient = multipliers[3]
+            tax_rate = multipliers[5]
 
-        # these values can selected from a database
-        special_edu_multiplier = 1.25
-        course_multiplier = 2
-        education_state_multiplier = 1
-        if self.ui.rb_graduate.isChecked():
-            education_state_multiplier = 1.05
-        elif self.ui.rb_doctorate.isChecked():
-            education_state_multiplier = 1.15
-        income_tax = 0.15
-        if self.ui.rb_20.isChecked():
-            income_tax = 0.2
-        elif self.ui.rb_27.isChecked():
-            income_tax = 0.27
-        elif self.ui.rb_35.isChecked():
-            income_tax = 0.35
-        obj_list = [self.obj_control(self.ui.le_normal_day) * price_coefficient * education_state_multiplier * day_coefficient,
-                    self.obj_control(self.ui.le_normal_night) * price_coefficient * education_state_multiplier * night_coefficient,
-                    self.obj_control(self.ui.le_normal_weekend) * price_coefficient * education_state_multiplier * night_coefficient,
-                    self.obj_control(self.ui.le_watch) * price_coefficient * education_state_multiplier * day_coefficient,
-                    self.obj_control(self.ui.le_course_day) * price_coefficient * education_state_multiplier * day_coefficient * course_multiplier,
-                    self.obj_control(self.ui.le_course_night) * price_coefficient * education_state_multiplier * night_coefficient * course_multiplier,
-                    self.obj_control(self.ui.le_course_weekend) * price_coefficient * education_state_multiplier * night_coefficient * course_multiplier,
-                    self.obj_control(self.ui.le_special_day) * price_coefficient * education_state_multiplier * day_coefficient * special_edu_multiplier,
-                    self.obj_control(self.ui.le_special_night) * price_coefficient * education_state_multiplier * night_coefficient * special_edu_multiplier,
-                    self.obj_control(self.ui.le_special_weekend) * price_coefficient * education_state_multiplier * night_coefficient * special_edu_multiplier
-                    ]
-        gross_total = sum(obj_list)
-        income_deduction = gross_total * income_tax
-        tax_deduction = gross_total * tax_rate
-        total_deduction = income_deduction + tax_deduction
-        net_price = gross_total - total_deduction
-        self.ui.le_gross.setText(str(round(gross_total, 2)) + " TL")
-        self.ui.le_deduction.setText(str(round(total_deduction, 2)) + " TL")
-        self.ui.le_net.setText(str(round(net_price, 2)) + " TL")
+            # these values can selected from a database
+            special_edu_multiplier = 1.25
+            course_multiplier = 2
+            education_state_multiplier = 1
+            if self.ui.rb_graduate.isChecked():
+                education_state_multiplier = 1.05
+            elif self.ui.rb_doctorate.isChecked():
+                education_state_multiplier = 1.15
+            income_tax = 0.15
+            if self.ui.rb_20.isChecked():
+                income_tax = 0.2
+            elif self.ui.rb_27.isChecked():
+                income_tax = 0.27
+            elif self.ui.rb_35.isChecked():
+                income_tax = 0.35
+            obj_list = [self.obj_control(self.ui.le_normal_day) * price_coefficient * education_state_multiplier * day_coefficient,
+                        self.obj_control(self.ui.le_normal_night) * price_coefficient * education_state_multiplier * night_coefficient,
+                        self.obj_control(self.ui.le_normal_weekend) * price_coefficient * education_state_multiplier * night_coefficient,
+                        self.obj_control(self.ui.le_watch) * price_coefficient * education_state_multiplier * day_coefficient,
+                        self.obj_control(self.ui.le_course_day) * price_coefficient * education_state_multiplier * day_coefficient * course_multiplier,
+                        self.obj_control(self.ui.le_course_night) * price_coefficient * education_state_multiplier * night_coefficient * course_multiplier,
+                        self.obj_control(self.ui.le_course_weekend) * price_coefficient * education_state_multiplier * night_coefficient * course_multiplier,
+                        self.obj_control(self.ui.le_special_day) * price_coefficient * education_state_multiplier * day_coefficient * special_edu_multiplier,
+                        self.obj_control(self.ui.le_special_night) * price_coefficient * education_state_multiplier * night_coefficient * special_edu_multiplier,
+                        self.obj_control(self.ui.le_special_weekend) * price_coefficient * education_state_multiplier * night_coefficient * special_edu_multiplier
+                        ]
+            gross_total = sum(obj_list)
+            income_deduction = gross_total * income_tax
+            tax_deduction = gross_total * tax_rate
+            total_deduction = income_deduction + tax_deduction
+            net_price = gross_total - total_deduction
+            self.ui.le_gross.setText(str(round(gross_total, 2)) + " TL")
+            self.ui.le_deduction.setText(str(round(total_deduction, 2)) + " TL")
+            self.ui.le_net.setText(str(round(net_price, 2)) + " TL")
+        except Exception as err:
+            QMessageBox.warning(self, "Dikkat", "Önce data girişleri yapmalısınız" + str(err))
 
     def cleaner(self):
         self.ui.rb_15.setChecked(True)
@@ -160,7 +163,7 @@ class MainWindow(QMainWindow):
         if gross != "" and deduction != "" and net != "":
             staff_list = ["-".join([str(i[2]), str(i[0])]) for i in self.connection.selector(f"""SELECT * FROM staffs""")]
             name, ok_pressed = QInputDialog.getItem(self, "Seçim Ekranı", "Kayıt yapmak istediğiniz personeli seçiniz.", staff_list, 0, False)
-            if ok_pressed:
+            if ok_pressed and name != "":
                 staff_id = name.split("-")[1]
                 counter = self.connection.find(f"""SELECT COUNT(id) FROM staffs_prices WHERE yearID={year_id} AND monthID={month_id} AND staffID={staff_id}""")
                 if counter == 0:
@@ -174,6 +177,8 @@ class MainWindow(QMainWindow):
                             f"""UPDATE staffs_prices SET gross={float(gross)}, deduction={float(deduction)}, net={float(net)}
                              WHERE staffID={staff_id} AND yearID={year_id} AND monthID={month_id} """)
                         QMessageBox.warning(self, "Dikkat", "Üzerine yazıldı")
+            else:
+                QMessageBox.warning(self, "Dikkat", "İşlem yapmak için kişi eklemelisiniz.")
         else:
             QMessageBox.warning(self, "Dikkat", "Ortada hesaplanan bir şey yok.")
 
